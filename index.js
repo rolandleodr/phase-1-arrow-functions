75415abb2a96b7a31b94a8f01b820961a98d36de
Roland Leo Del Rosario <rleoadr@gmail.com>
2022-03-25 22:21:56 -0500

    Completed assignment

---
+++ index.js
@@ -0,0 +1,13 @@
+const divide = function() {
+    return 2000 / 100;
+}
+divide(2000/100)
+
+const add = (num1, num2) => {
+    return num1 + num2
+}
+add    (2,4)
+const square = (num1) => {
+    return num1 * num1
+}
+square(2)
